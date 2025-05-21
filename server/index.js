7368c5cc1aa7748edfc5f268fdb096fdf7ff98e2
ProgAbhi <abhishekpandey678@gmail.com>
2025-05-21 11:28:48 +0530

    Updated Server for deployment

--- server/index.js
+++ server/index.js
@@ -18,10 +18,12 @@ app.use("/api/posts", postRoutes);
 app.use("/api/upload", uploadRoutes);
 
 // MongoDB Connection
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log("Server running on port", process.env.PORT);
+    app.listen(PORT, () => {
+      console.log("Server running on port", PORT);
     });
   })
   .catch(err => console.log(err));
